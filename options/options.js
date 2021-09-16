96829b0c406b3b059417acd692d8b41a68878bf1
Crowdbotics <team@crowdbotics.com>
2021-09-16 23:39:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "long_mountain_30626Identifier",
+  url: "https://long_mountain_30626Identifier.botics.co",
+  api: "https://long_mountain_30626Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
